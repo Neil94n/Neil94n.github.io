043deedb0bd2f5d8fd9b5c94530b808914026be6
rookie <417095066@qq.com>
2018-11-12 15:38:17 +0800

    Site updated: 2018-11-12 15:38:13

--- js/src/motion.js
+++ js/src/motion.js
@@ -1,19 +1,17 @@
 /* global NexT: true */
 
-$(document).ready(function () {
-    window.onload=function(){
-      var bwol=document.body.offsetWidth;
-        if(bwol < 975){
-           $('aside#sidebar').css("display","none");
-           $('body').css("paddingRight","0px");
-       }
-     }
-     window.onresize = function(){
-     var bwos=document.body.offsetWidth;
-     bwos < 975 && $('body').velocity('stop').velocity({paddingLeft: 0},0);
-      if($('aside#sidebar').css('display') != 'none' && $('aside#sidebar').css('width')!='0px')
-         $('body').velocity('stop').velocity({paddingLeft: 350},0);
-       
+$(document).ready(function () {window.onload=function(){
+  var bwol=document.body.offsetWidth;
+  if(bwol < 975){
+      $('aside#sidebar').css("display","none");
+      $('body').css("paddingLeft","0px");
+  }
+}
+window.onresize = function(){
+  var bwos=document.body.offsetWidth;
+  bwos < 975 && $('body').velocity('stop').velocity({paddingLeft: 0},0);
+  if($('aside#sidebar').css('display') != 'none' && $('aside#sidebar').css('width')!='0px')
+      $('body').velocity('stop').velocity({paddingLeft: 350},0);
 }
   NexT.motion = {};
 
@@ -112,7 +110,7 @@ $(document).ready(function () {
       $(document)
         .on('sidebar.isShowing', function () {
           NexT.utils.isDesktop() && $('body').velocity('stop').velocity(
-            {paddingRight: SIDEBAR_WIDTH},
+            {paddingLeft: SIDEBAR_WIDTH},
             SIDEBAR_DISPLAY_DURATION
           );
         })
